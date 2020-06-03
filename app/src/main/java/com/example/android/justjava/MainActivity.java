14cd4687cf89310e83c64eb4b35119d449ae7d14
UnaLuz <luz.cucarella97@gmail.com>
2020-06-03 11:05:10 -0300

    Changed name of variables "coffeeCount" and "coffeePrice" to "cupsCount" and "cupPrice"
    Deleted the manual casting for findViewById method since its not required anymore

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.android.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected int coffeeCount = 0;
-    protected int coffeePrice = 80;
+    protected int cupsCount = 0;
+    protected int cupPrice = 80;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +21,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        //displayPrice(coffeeCount * coffeePrice);
-        String message = "That'll be $" + calculatePrice(coffeeCount, coffeePrice) + "\nThank you!";
+        String message = "That'll be $" + calculatePrice(cupsCount, cupPrice) + "\nThank you!";
         displayMessage(message);
     }
 
@@ -43,38 +40,30 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        coffeeCount += 1;
-        display(coffeeCount);
+        cupsCount += 1;
+        display(cupsCount);
     }
 
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        if (coffeeCount > 0) {
-            coffeeCount -= 1;
+        if (cupsCount > 0) {
+            cupsCount -= 1;
         }
-        display(coffeeCount);
+        display(cupsCount);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText(NumberFormat.getInstance().format(number));
     }
 
-    /**
-     * This method displays the price value on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
     private void displayMessage(String m) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = findViewById(R.id.price_text_view);
         priceTextView.setText(m);
     }
 }
